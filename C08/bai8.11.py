a015f9373eeb8dd699254fb9c5f48daac19bedb8
Linh0001 <117285944+Linh0001@users.noreply.github.com>
2022-11-25 14:00:16 +0700

    Bài tập chương 8

--- C08/bai8.11.py
+++ C08/bai8.11.py
@@ -1,8 +1,9 @@
 #Nhập vào số nguyên n
 print("Nhập n:")
 n=int(input())
-#Nhập vào số thực x3
+#Nhập vào số thực x
 print("Nhập x:")
 x=float(input())
+# Tính A
 A=(((x*x)+x+1)**n)+(((x*x)-x+1)**n)
 print("A=(x*x +x+1)^n + (x*x -x + 1)^n =",A)
