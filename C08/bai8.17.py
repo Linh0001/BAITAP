708eec3a9e90b0109d1415245dced06640b35708
Linh0001 <117285944+Linh0001@users.noreply.github.com>
2022-11-25 13:59:02 +0700

    Bài tập chương 8

--- C08/bai8.17.py
+++ C08/bai8.17.py
@@ -1,8 +1,7 @@
 #Tính BCNN(a,b)a,b nhập từ bàn phím
 a=int(input('a = '))
 b=int(input('b = '))
-# Cho 1 biến chạy từ giá trị nhỏ nhất của 1 số ngược về 1 nếu 
-# cả 2 số đều chia hết cho biến đó thì là ucln 
+# Tìm UCLN
 for i in range(min(a,b),0,-1):
     if a%i==0 and b%i==0:
         x=int((a*b)/i)   #BCNN(a,b)=(a*b)/UCLN(a,b)
