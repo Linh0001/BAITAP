3c98f8f955d8b3f05475c06c1bb27dafbd194ce3
Linh0001 <117285944+Linh0001@users.noreply.github.com>
2022-12-01 15:44:42 +0700

    Update Bai9.9.py

--- C09/Bai9.9.py
+++ C09/Bai9.9.py
@@ -1,20 +1,13 @@
 # Sử dụng biểu thức lamda để tính diện tích,chu vi hình tròn tham số r (bán kính)
 import math
-def hinh_tron(r):
-    sht =math.pi*math.pow(r,2)
-    pht =2*math.pi*r
-    return sht,pht
-def hinh_chu_nhat(a,b):
-    scn=a*b
-    pcn=2*(a+b)
-    return scn,pcn
+sht =lambda r:math.pi*math.pow(r,2)
+pht =lambda r:2*math.pi*r
+scn=lambda a,b:a*b
+pcn=lambda a,b:2*(a+b)
 r=float(input("Bán kính r = "))
 a=float(input("Chiều dài hình chữ nhật a = "))
 b=float(input("Chiều rộng hình chữ nhật b = "))
-sht,pht=hinh_tron(r)
-scn,pcn=hinh_chu_nhat(a,b)
-print("S hình tròn = %0.2f"%sht)
-print("P hình tròn = %0.2f"%pht)
-print("S hình chữ nhật = %0.2f"%scn)
-print("P hình chữ nhật = %0.2f"%pcn)
-
+print("S hình tròn = %0.2f"%sht(r))
+print("P hình tròn = %0.2f"%pht(r))
+print("S hình chữ nhật = %0.2f"%scn(a,b))
+print("P hình chữ nhật = %0.2f"%pcn(a,b))
